121354b5e68e073b2dc2230ec0578db19e8f2222
JAndrade96 <jcjunior96.jcab@gmail.com>
2022-09-05 14:22:39 -0400

    update climate

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import './App.css'
 import Climate from './components/Climate'
 import Loading from './components/Loading'
 
-document.body.style = `background: url(../src/assets/img/cloud.jpg) no-repeat center center fixed;
--webkit-background-size: cover;
-background-size: 100% 100%;`
 
 function App() {
   const [count, setCount] = useState(0)
